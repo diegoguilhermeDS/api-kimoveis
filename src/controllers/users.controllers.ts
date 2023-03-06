90293e77aa0a71c0686d73fde411b2800b82eb34
Diego Guilherme <diegoguilherme752@oultook.com>
2023-03-06 19:07:45 -0300

    feature: update user

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { iUserUpdate } from "../interfaces/users.interfaces";
 import createUserService from "../services/users/createUser.services";
 import listUserService from "../services/users/listUsers.services";
+import updateUserService from "../services/users/updateUser.services";
 
 const createUserControllers = async (req: Request, res: Response) => {
 
@@ -19,7 +21,18 @@ const listUsersController = async (req: Request, res: Response) => {
 
 }
 
+const updateUserControllers = async (req: Request, res: Response) => {
+
+    const newUserData: iUserUpdate = req.body
+    const userId: number = parseInt(req.params.id)
+
+    const newUser = await updateUserService(newUserData, userId)
+
+    return res.json(newUser)
+}
+
 export {
     createUserControllers,
-    listUsersController
+    listUsersController,
+    updateUserControllers
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { createUserControllers, listUsersController } from "../controllers/users.controllers";
+import { createUserControllers, listUsersController, updateUserControllers } from "../controllers/users.controllers";
 import ensuresDataISValid from "../middlewares/ensuresDataIsValid.middlewares";
 import ensuresEmailNotExists from "../middlewares/ensuresEmailNotExist.middlewares";
+import ensuresIdUserNotExists from "../middlewares/ensuresIdUserNotExists.middlewares";
 import ensuresTokenIsValid from "../middlewares/ensuresTokenIsValid.middlewares";
 import ensuresUserIsAdmin from "../middlewares/ensuresUserIsAdmin.middlewares";
-import { userSchema } from "../schemas/users.schemas";
+import { userSchema, updateUserSchema } from "../schemas/users.schemas";
 
 const usersRouter: Router = Router()
 
 usersRouter.post("", ensuresDataISValid(userSchema), ensuresEmailNotExists, createUserControllers)
 usersRouter.get("", ensuresTokenIsValid, ensuresUserIsAdmin, listUsersController)
+usersRouter.patch("/:id", ensuresDataISValid(updateUserSchema), ensuresIdUserNotExists, ensuresTokenIsValid, ensuresUserIsAdmin, updateUserControllers)
 
 
 export default usersRouter
